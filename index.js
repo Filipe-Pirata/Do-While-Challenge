8faca746a2908705f60e891b23af4192dae95a1f
Filipe Azevedo <93743386+Filipe-Pirata@users.noreply.github.com>
2021-12-12 02:30:48 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+document
+  .querySelector('.who')
+  .addEventListener('click', () =>
+    document.querySelector('.show').classList.toggle('screenhiden')
+  )
+document
+  .querySelector('.why')
+  .addEventListener('click', () =>
+    document.querySelector('.show2').classList.toggle('screenhiden2')
+  )
+document
+  .querySelector('.benefits')
+  .addEventListener('click', () =>
+    document.querySelector('.show3').classList.toggle('screenhiden3')
+  )
+document
+  .querySelector('.howitwork')
+  .addEventListener('click', () =>
+    document.querySelector('.show4').classList.toggle('screenhiden4')
+  )
+document
+  .querySelector('.partners')
+  .addEventListener('click', () =>
+    document.querySelector('.show5').classList.toggle('screenhiden5')
+  )
